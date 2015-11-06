5e52d30eea858bf0f0eb1aa0a8b153e39b1df829
cesar-giraldo <cesargiraldo1108@gmail.com>
2015-11-06 17:45:56 -0500

    implementación de boostrap 3.35

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -17,6 +17,7 @@ class AppKernel extends Kernel
             new Doctrine\Bundle\DoctrineBundle\DoctrineBundle(),
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
             new Kijho\MailerBundle\KijhoMailerBundle(),
+            new Fkr\CssURLRewriteBundle\FkrCssURLRewriteBundle(),//Reescritura de rutas en css
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'))) {
