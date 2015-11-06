ebcfbe580fa423f7a87ae7649ba6c815bc2503ba
cesar-giraldo <cesargiraldo1108@gmail.com>
2015-11-06 12:01:39 -0500

    seteo de los parámetros del bundle como parámetros del container

--- src/Kijho/MailerBundle/Controller/LayoutController.php
+++ src/Kijho/MailerBundle/Controller/LayoutController.php
@@ -10,6 +10,9 @@ class LayoutController extends Controller
     {
         $em = $this->getDoctrine()->getManager();
         
+        $layoutStorage = $this->container->getParameter('kijho_mailer.layout_storage');
+        var_dump($layoutStorage." este es");
+        
         $layouts = $em->getRepository('MasterUnlockBackendBundle:EmailLayout')->findAll();
         
         return $this->render('KijhoMailerBundle:Layout:index.html.twig', array('layouts' => $layouts));
--- src/Kijho/MailerBundle/DependencyInjection/KijhoMailerExtension.php
+++ src/Kijho/MailerBundle/DependencyInjection/KijhoMailerExtension.php
@@ -21,9 +21,9 @@ class KijhoMailerExtension extends Extension {
         $configuration = new Configuration();
         $config = $this->processConfiguration($configuration, $configs);
 
-//        foreach ($config as $key => $value) {
-//            $container->setParameter('ewz_recaptcha.'.$key, $value);
-//        }
+        foreach ($config as $key => $value) {
+            $container->setParameter('kijho_mailer.'.$key, $value);
+        }
         
         $loader = new Loader\YamlFileLoader($container, new FileLocator(__DIR__ . '/../Resources/config'));
         $loader->load('services.yml');
